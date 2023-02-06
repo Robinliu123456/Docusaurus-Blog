19881d9abcd765147453c91fcb376d0d3b883d5d
Robinliu123456 <1500529741@qq.com>
2023-02-06 14:18:51 +0800

    fix:locact.js插件定位配置

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+    plugins: [
+      ["@locator/babel-jsx/dist", {
+        env: "development"
+      }]
+    ],
 };
